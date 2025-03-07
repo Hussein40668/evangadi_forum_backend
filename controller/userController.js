33fcbe1fd5f39f04ad89894e88308a3f27d2db0b
Hussein Muhammed <Hussein.Muhammed091203@gmail.com>
2025-03-07 08:03:38 +0300

    Login user controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,3 @@
-const e = require("express");
 const dbConnection = require("../model/dbModel");
 const bycrypt = require("bcrypt");
  const { StatusCodes } = require('http-status-codes');
@@ -41,8 +40,44 @@ const register = async (req, res) => {
   }
 }
 
-const login = (req, res) => {
-  res.send("user loggedin");
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(StatusCodes.BAD_REQUEST).json({msg:"please provide all required information"});
+  }
+
+  try {
+    const [user] = await dbConnection.query("SELECT username,userid,password FROM users WHERE email = ? ", [email]);
+
+   
+    if (user.length === 0) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "invalid credentials" });
+    }
+
+    // compare the password
+    await bycrypt.compare(password, user[0].password);
+
+    const isMatch = await bycrypt.compare(password, user[0].password);
+    if (!isMatch) { return res.status(StatusCodes.BAD_REQUEST).json({ msg: "invalid credentials" });
+    }
+    // return res.json({user});
+    
+    // const payload = {
+    //   user: {
+    //     id: user[0].userid,
+    //   },
+    // };
+
+    // jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" }, (err, token) => {
+    //   if (err) throw err;
+    //   res.status(StatusCodes.OK).json({ token });
+    // });
+    res.status(StatusCodes.OK).json({msg:"user logged in successfully"});
+  } catch (error) {
+    console.log(error.message);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"something went wrong,try again later!"});
+  }
 };
 
 const checkUser = (req, res) => {
